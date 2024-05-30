005b4019523255d9d65901b5e738470cdc48f07a
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2024-05-30 14:15:57 +0900

    Create SplashActivity.java

---
+++ SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.termproject_parkinglot_10;
+
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.Handler;
+import android.content.Intent;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        moveMain(1);
+    }
+
+    private void moveMain(int sec) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000*sec);
+    }
+}
